34efea775f4f61d9c5bda46f351fda6f9e93a6a5
Abhik Baidya <abhikbaidya7@gmail.com>
2024-09-13 20:04:21 +0530

    app commit

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Home = () => {
+export default function Page() {
+  const { user } = useUser();
+
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
     </SafeAreaView>
   );
-};
-
-export default Home;
+}
